7e5e29daab9c29b06f19d2385268a6aa1c9eaf39
Rezonation <TheAoihoshi@gmail.com>
2013-11-06 18:07:42 +0200

    Collision damage partially implemented

--- MageClash/src/Dev/Rezo/Mageclash/Controller/ArenaContactListener.java
+++ MageClash/src/Dev/Rezo/Mageclash/Controller/ArenaContactListener.java
@@ -1,16 +1,30 @@
 package Dev.Rezo.Mageclash.Controller;
 
+import Dev.Rezo.Mageclash.Model.Entity;
+import Dev.Rezo.Mageclash.Model.PlayerEntity;
+
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class ArenaContactListener implements ContactListener {
+Entity fixA;
+Entity fixB;
 
 	@Override
 	public void beginContact(Contact contact) {
 		// TODO Auto-generated method stub
-		
+		fixA = (Entity) contact.getFixtureA().getUserData();
+		fixB = (Entity) contact.getFixtureB().getUserData();
+		if (fixA instanceof PlayerEntity){
+			((PlayerEntity) fixA).HEALTH = ((PlayerEntity) fixA).HEALTH - 20;
+			if (((PlayerEntity) fixA).HEALTH < 0){
+			}
+		}
+		if (fixA instanceof PlayerEntity){
+			((PlayerEntity) fixA).HEALTH = ((PlayerEntity) fixA).HEALTH - 20;
+		}
 	}
 
 	@Override
